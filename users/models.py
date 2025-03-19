bac2f0387cf883b274a7a0cec95cfeda50c98d5c
Shamil Lachenilaev <lachen1305@gmail.com>
2025-03-19 09:01:02 +0300

    Added the Users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# NULLABLE = {'null': True, 'blank': True}
+NULLABLE_FOR_STRING = {'null': False, 'blank': True}
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, verbose_name='phone number', **NULLABLE_FOR_STRING)
+    telegram = models.CharField(max_length=150, verbose_name='telegram username', **NULLABLE_FOR_STRING)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.email}'
