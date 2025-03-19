399b908998eb66660121e60f223ba7cb9fb707a8
Shamil Lachenilaev <lachen1305@gmail.com>
2025-03-19 15:57:59 +0300

    Update users/models

--- users/models.py
+++ users/models.py
@@ -11,6 +11,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='email')
     phone = models.CharField(max_length=35, verbose_name='phone number', **NULLABLE_FOR_STRING)
     telegram = models.CharField(max_length=150, verbose_name='telegram username', **NULLABLE_FOR_STRING)
+    is_active = models.BooleanField(default=True, verbose_name='active')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
