bb7d9921706a050ab90f26b5fbb51897d32ec2b5
Shamil Lachenilaev <lachen1305@gmail.com>
2025-03-22 16:39:49 +0300

    Added NULLABLE to users app

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# NULLABLE = {'null': True, 'blank': True}
+NULLABLE = {'null': True, 'blank': True}
 NULLABLE_FOR_STRING = {'null': False, 'blank': True}
 
 
