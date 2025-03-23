6d27f9e62c4d889c0d64eada91bbd2f9efc11652
Shamil Lachenilaev <lachen1305@gmail.com>
2025-03-23 15:38:16 +0300

    Setting up routes for: dogs.html in the dogs app

--- dogs/urls.py
+++ dogs/urls.py
@@ -3,6 +3,7 @@ from dogs.views import (
     index,
     breeds_list,
     breed_dogs_list,
+    dogs_list,
 )
 from dogs.apps import DogsConfig
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path("", index, name="index"),
     path("breeds/", breeds_list, name="breeds"),
     path("breeds/<int:pk>/dogs/", breed_dogs_list, name="breed_dogs"),
+    path("dogs/", dogs_list, name="dogs_list"),
 ]
