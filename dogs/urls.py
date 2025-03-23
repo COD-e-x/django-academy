a54390d1da2d726a3e1842fd1ff9738c983e6a52
Shamil Lachenilaev <lachen1305@gmail.com>
2025-03-23 16:12:46 +0300

    Setting up routes for: create.html in the dogs app

--- dogs/urls.py
+++ dogs/urls.py
@@ -4,6 +4,7 @@ from dogs.views import (
     breeds_list,
     breed_dogs_list,
     dogs_list,
+    dog_create,
 )
 from dogs.apps import DogsConfig
 
@@ -14,4 +15,5 @@ urlpatterns = [
     path("breeds/", breeds_list, name="breeds"),
     path("breeds/<int:pk>/dogs/", breed_dogs_list, name="breed_dogs"),
     path("dogs/", dogs_list, name="dogs_list"),
+    path("dogs/create/", dog_create, name="dog_create"),
 ]
